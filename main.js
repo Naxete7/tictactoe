8c6f70d657d37615c9c4f0a6943eaf4933de5bbf
Ignacio <naxete7@hotmail.es>
2022-10-13 18:51:49 +0200

    pantalla de ganador linkeada

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
+document.querySelectorAll('.botonp').forEach(
+    (obj, i) => obj.addEventListener('click', (e) => btnPulsado(e, i)))
+
 let turno = 0
 const tablero = [];
-
-
 
 
 const btnPulsado = (e, pos) => {
     turno++;
     const btn = e.target;
     const imagen = turno % 2 ? 'url(../img/dragon-ball-goku-png-photos-818491-1.png)' : 'url(../img/royal-blue-vegeta-4464151.png)'
-    btn.style.backgroundImage = imagen
+    btn.style.backgroundImage = imagen;
     tablero[pos] = imagen;
-    if (ganador()) alert("Enhorabuena!");
+    if (ganador()) window.location.href = "ganador.html";
 };
 
-document.querySelectorAll('.botonp').forEach(
-    (obj, i) => obj.addEventListener('click', (e) => btnPulsado(e, i)))
 
 const ganador = () => {
     if (tablero[0] == tablero[1] && tablero[0] == tablero[2] && tablero[0]) {
@@ -40,36 +39,33 @@ const ganador = () => {
 };
 
 
+
 const guardarDatos = () => {
     listaJugadores = [{
-            nombre: namePlayer1.value,
-            tipo: human1.value
-        },
-        {
-            nombre: namePlayer2.value,
-            tipo: cpu1.value
-        }
-    ]
-    tipojugador = [{
+                nombre: namePlayer1.value,
+                tipo: jugador1.value
+            },
+            {
+                nombre: namePlayer2.value,
+                tipo: human2.value
 
-        tipo: human1.value,
-        tipo: cpu1.value,
-        tipo: human2.value,
-        tipo: cpu2.value
-    }]
+            }
+        ]
+        // tipojugador = [{
 
+    //     tipo: human1.value,
+    //     tipo: cpu1.value,
+    //     tipo: human2.value,
+    //     tipo: cpu2.value
+    // }]
+    //guardamos datos en sessionStorage
     sessionStorage.setItem('LISTA_JUGADORES', JSON.stringify(listaJugadores));
 };
-
-
-
-
-
-
-
-
-// {
-//     $("#btnganador").click(); {
+//recuperamos datos del sessionStorage
+let jug1Data = JSON.parse(sessionStorage.getItem('playerX'))
+let jug2Data = JSON.parse(sessionStorage.getItem('playerO'))
+    // {
+    //     $("#btnganador").click(); {
 
 //         window.location.href = "./html/ganador.html";
 
@@ -99,7 +95,7 @@ const guardarDatos = () => {
 // casillas.map((casilla, /*index*/ ) => {
 //     casilla.addEventListener("click", () => {
 //         if (casilla.innerHTML == "") {
-//             casilla.innerHTML = (interruptor) ? 'url(../img/dragon-ball-goku-png-photos-818491-1.png)' : 'O';
+//             casilla.innerHTML = (interruptor) ? 'x' : 'O';
 //             interruptor = !interruptor;
 
 //             if (ganador()) alert("Enhorabuena!");
@@ -107,3 +103,16 @@ const guardarDatos = () => {
 //         };
 //     });
 // });
+
+// let jugador1 = document.getElementById(namePlayer1)
+// let jugador2 = document.getElementById(namePLayer2)
+
+
+
+// let player1 = { nombre: jugador1.value } //tipo: jugador1.value}
+// let player2 = { nombre: jugador2.value } //tipo: jugadpr2.value }
+// let player = {}
+
+
+// sessionStorage.setItem('playerX', JSON.stringify(player1))
+// sessionStorage.setItem('playerO', JSON.stringify(player2))
