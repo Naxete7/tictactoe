15c5fc96e40da560f1b326b5a9b7a44a02fe373a
Ignacio <naxete7@hotmail.es>
2022-10-21 11:58:41 +0200

    cambio de imagen en celda arreglado

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
+//falta limitar a 3 fichas por jugador. Poner nombre de ganador. CPU
+// validar si playerX tiene 3 fichas, no dejar poner mas
+
+
+
+
 document.querySelectorAll('.botonp').forEach(
     (btnp, i) => btnp.addEventListener('click', (e) => btnPulsado(e, i)))
+
+
 
 let tirada = 0
 let tablero = [];
 let playerX = 'url(../img/dragon-ball-goku-png-photos-818491-1.png)'
 let playerO = 'url(../img/royal-blue-vegeta-4464151.png)'
-    // let contPlayerX = < 3;
-    // let contPlayerO = < 3;
+
+let contPlayerX = 3
+let contPlayerO = 3
 
 //Guardamos datos de los jugadores en la sessionStorage
 const optionHumanJ1 = document.getElementById('human1');
 const optionHumanJ2 = document.getElementById('human2');
+// class Player {
+//     constructor(nombre, tipo, mark) {
+//         this.nombre = nombre;
+//         this.tipo = tipo;
+//         this.turnos = 3;
+//         this.mark = mark
+//     }
+// };
 
 
 const guardarDatos = () => {
@@ -46,52 +63,44 @@ function turno() {
     }
 };
 
+// const fichasX = () => {
+
+//     if (contPlayerX < 3) { return true } else { return false }
+// };
+
+// const fichasO = () => {
+
+//     if (contPlayerO < 3) { return true } else { return false }
+
+// };
 
 
-//programamos la cpu modo random
-
-function cpu() {
-    If(humanJ1 = false); {
-
-        humanJ2 == true
-
-    };
-}
-
-
-const cpuRandom = () => {
-    const random = Math.round(Math.random() * 8)
-    return casillas[random]
-}
 
 
 
 //Preparamos el juego para que al pulsar el boton salgan las fichas
 
+
+
 const btnPulsado = (e, pos) => {
-    turno()
-    tirada++;
-    const btn = e.target;
-    //if la posicion esta vacia me deja, sino no hace nada
-    if (tablero.pos = [""]) {
+    if (playerX) { contPlayerX--; };
+    contPlayerO--;
+    // if (tablero[pos] == `div.rectangulo.celda${pos}`) {
+    if (tablero[pos] == undefined) {
+        turno()
+        tirada++;
+        const btn = e.target;
+        //if la posicion esta vacia me deja, sino no hace nada
+        // console.log(tablero[pos] == undefined);
         const imagen = tirada % 2 ? playerX : playerO
         btn.style.backgroundImage = imagen;
         tablero[pos] = imagen;
+        // console.log(contPlayerX);
+        // console.log(contPlayerX);
+        // };
+        if (ganador()) window.location.href = "ganador.html";
 
-
-        //Cada jugador puede tener como maximo 3 fichas en el tablero
-
-        // if (X = < 3)
-        playerX.tirada <= 3
-        playerO.tirada <= 3
-            // if (O = < 3)
-
-
-        //si es diferente de null, y diferente del otro jugador, si puedes pulsar
-        // if (X = !null && = !O)) {
     };
-    if (ganador()) window.location.href = "ganador.html";
-    // winner()
 }
 
 //Comprobamos si hay ganador
@@ -102,7 +111,7 @@ const ganador = () => {
         return true;
     } else if (tablero[6] == tablero[7] && tablero[6] == tablero[8] && tablero[6]) {
         return true;
-    } else if (tablero[30] == tablero[3] && tablero[0] == tablero[6] && tablero[0]) {
+    } else if (tablero[0] == tablero[3] && tablero[0] == tablero[6] && tablero[0]) {
         return true;
     } else if (tablero[1] == tablero[4] && tablero[1] == tablero[7] && tablero[1]) {
         return true;
@@ -118,13 +127,39 @@ const ganador = () => {
 
 };
 
-//Funcion de ganador para que aparezca el nombre del ganador en el cuadro
-// let cuadroGanador = document.getElementById("cuadroganador")
 
-// function winner() {
-//     if (ganador = playerX) {
-//         cuadroGanador.innerHTML = "Ha ganado la partida " + name1
-//     } else {
-//         cuadroGanador.innerHTML = "Ha ganado la partida" + name2
-//     }
-// };
+//volvemos a guardar en sessionStorage para que salga en el cuadro de ganador
+
+const winnernHumanJ1 = document.getElementById('human1');
+const winnerHumanJ2 = document.getElementById('human2');
+
+
+const winner = () => {
+    playersList = [{
+            name: namePlayer1.value,
+            human: optionHumanJ1.checked
+        },
+        {
+            name: namePlayer2.value,
+            human: optionHumanJ2.checked
+        }
+    ]
+    sessionStorage.setItem('LISTA_GANADORES', JSON.stringify(playersList));
+};
+//recuperamos datos del sessionStorage
+let winnerJ1 = JSON.parse(sessionStorage.getItem("LISTA_JUGADORES"))[0].name;
+let winnerJ2 = JSON.parse(sessionStorage.getItem("LISTA_JUGADORES"))[1].name;
+
+
+//Funcion de ganador para que aparezca el nombre del ganador en el cuadro
+
+
+let cuadroGanador = document.getElementById("cuadroganador")
+
+function win() {
+    if (ganador = humanJ1) {
+        cuadroGanador.innerHTML = "Ha ganado la partida " + name1
+    } else {
+        cuadroGanador.innerHTML = "Ha ganado la partida" + name2
+    }
+};
